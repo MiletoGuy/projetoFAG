ccb24fff7921d046efc121d2af206f8de84c34cb
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2022-05-17 19:15:02 -0300

    Adicionando a rota usuarios

---
+++ new/api/routes/usuarios.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+router.get('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM USUARIO',
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(201).send({response: resultado})
+            }
+        )
+    })
+});
+
+
+module.exports = router;
