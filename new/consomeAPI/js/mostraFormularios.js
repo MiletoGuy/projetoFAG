5611c4a563dd28bd9ff12feb10757cf0705a3a3b
luizaugusto <luizaugustom030502@gmail.com>
2022-06-23 12:41:58 -0300

    Mostrando o email corretamente

--- new/consomeAPI/js/mostraFormularios.js
+++ new/consomeAPI/js/mostraFormularios.js
@@ -12,7 +12,7 @@ async function fetchFormularios() {
     }
 }
 
-function listaMensagem(formularioContainerElementId) {
+function listaEmail(formularioContainerElementId) {
     const formularioContainerElement = document.getElementById(formularioContainerElementId);
 
     if (!formularioContainerElement) {
@@ -27,9 +27,7 @@ function listaMensagem(formularioContainerElementId) {
         }
 
         for(const formulario of formularios) {
-            let form = JSON.parse(formulario.FORM_FORMULARIO);
-            formularioContainerElement.innerHTML = form.Mensagem + '. Contato: ' + form.Telefone;
-            break;
+            formularioContainerElement.appendChild(mensagemElement(formulario))
         }
     })
     .catch((e) => {
@@ -37,18 +35,47 @@ function listaMensagem(formularioContainerElementId) {
     })
 }
 
-/*function mensagemElement(formulario) {
-    const anchorElement = document.createElement('p');
+function mensagemElement(formulario) {
     let form = JSON.parse(formulario.FORM_FORMULARIO);
-    anchorElement.innerText = form.Mensagem + ' ' + form.Telefone;
+    const emailContainer = document.createElement('div');
+    emailContainer.setAttribute("class", "email");
+    const linha1 = document.createElement('div');
+    linha1.setAttribute("class", "linha1");
+    const linha = document.createElement('div');
+    linha.setAttribute("id", "linha");
+    const svg = document.createElement('svg');
+    svg.setAttribute("xmlns","http://www.w3.org/2000/svg");
+    svg.setAttribute("width","14" );
+    svg.setAttribute("style","color:#05683E");
+    svg.setAttribute("height", "14");
+    svg.setAttribute("fill", "currentColor");
+    svg.setAttribute("class", "bi bi-circle-fill");
+    svg.setAttribute("viewBox", "0 0 16 16");
+    const circle = document.createElement('circle');
+    circle.setAttribute("cx", "8");
+    circle.setAttribute("cy", "8");
+    circle.setAttribute("r", "8");
+    const emailName = document.createElement('div');
+    emailName.setAttribute("class", "d-flex emailname")
+    emailName.innerText = form.nome;
+    const emailDate = document.createElement('div');
+    emailDate.setAttribute("class", "emaildate");
+    emailDate.innerText = "25/05/2022";
+    const emailMensagem = document.createElement('div');
+    emailMensagem.setAttribute("class", "emailcontent");
+    emailMensagem.innerText = form.Mensagem + '. Contato ' + form.Telefone 
+    svg.appendChild(circle);
+    linha1.appendChild(svg);
+    linha1.appendChild(emailName);
+    linha1.appendChild(emailDate);
+    emailContainer.appendChild(linha1);
+    emailContainer.appendChild(emailMensagem);
+    emailContainer.appendChild(linha);
 
-    const formularioMensagemElement = document.createElement('p');
-    formularioMensagemElement.appendChild(anchorElement);
-    
-    return formularioMensagemElement;
-}*/
+    return emailContainer;
+}
 
-function listaAutor(formularioContainerElementId) {
+/*function listaAutor(formularioContainerElementId) {
     const formularioContainerElement = document.getElementById(formularioContainerElementId);
 
     if (!formularioContainerElement) {
@@ -73,7 +100,7 @@ function listaAutor(formularioContainerElementId) {
     })
 }
 
-f/*unction autorElement(formulario) {
+function autorElement(formulario) {
     const anchorElement = document.createElement('p');
     let form = JSON.parse(formulario.FORM_FORMULARIO);
     anchorElement.innerText = form.nome;
